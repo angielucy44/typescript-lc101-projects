f31a1d88ad5b729b4e2de537ad6851d5ebdee638
Angie Zarinelli <angie.zarinelli@gmail.com>
2022-07-13 20:15:59 -0500

    first commit

---
+++ studio/Cargo.ts
@@ -0,0 +1,14 @@
+export class Cargo {
+    // properties and methods
+ }
+
+//  Defined in Cargo.ts
+// Implements the Payload interface
+// Properties
+// massKg should be a number.
+// material should be a string.
+// Constructor
+// Parameter massKg should be a number.
+// Parameter material should be a string.
+// Sets value of this.massKg and this.material
+
