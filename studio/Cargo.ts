a8da808f08e18c8efc9daca5a310e531405bb149
Angie Zarinelli <angie.zarinelli@gmail.com>
2022-07-14 20:00:38 -0500

    after Rowan studio

--- studio/Cargo.ts
+++ studio/Cargo.ts
@@ -1,14 +1,11 @@
-export class Cargo {
-    // properties and methods
- }
+export class Cargo implements Payload {
+    massKg: number
+    material: string
+    constructor(massKg: number, material: string){
+        this.massKg = massKg
+        this.material = material
+    }
+}
 
-//  Defined in Cargo.ts
-// Implements the Payload interface
-// Properties
-// massKg should be a number.
-// material should be a string.
-// Constructor
-// Parameter massKg should be a number.
-// Parameter material should be a string.
-// Sets value of this.massKg and this.material
+
 
